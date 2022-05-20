e7ecfcba7764efc238406859a46795f6e792abaa
lgbouma <bouma.luke@gmail.com>
2022-05-20 01:26:53 -0400

    [WIP] Cep-Her recovery debugging
    KOI-7368 recovery fails with TLS
    ....  but succeeds with astropy BLS
    
    this is b/c TLS fails to find any proposed periods or transit durations
    that improve the fit beyond a line.
    
    this surprising behavior
    
    A) scales with the amplitudes of the signals, e.g.,
    Rp/R* = [4e-2, 3e-2, 2e-2, 1e-2, 5e-3, 2e-3]
    pass, pass, fail, fail, fail, fail
    for an injected Kepler-1627 analog.
    
    B) gets worse with more data points, e.g., fixing Rp/R*=2e-2,
    and letting the number of points vary:
    npoints included = [1e3, 3e3, 5e3, 7e3, 1e4, 2e4, 4e4]
    [y, y, y, y, y, n, n, n].
    
    It might be good to raise a TLS bug report on this.
    Practically, the easiest fix is likely to move to BLS.

--- tests/test_recover_cepher_planets.py
+++ tests/test_recover_cepher_planets.py
@@ -2,7 +2,7 @@
 Q1: Can you find Kepler-1627, Kepler-1643, KOI-7913, and KOI-7368?
 Q2: How sensitive is this search?
 """
-import os
+import os, pickle
 import numpy as np, matplotlib.pyplot as plt
 from aeons.getters import get_kepler_lightcurve
 from aeons.processing import mask_quality_and_stitch
@@ -18,10 +18,15 @@ from cdips.lcproc.find_planets import (
 def main():
 
     star_ids = [
-        'Kepler-1627', 'Kepler-1643', 'KOI-7368', 'KOI-7913'
+        #'Kepler-1627', 'Kepler-1643', 'KOI-7368', 'KOI-7913'
+        'KOI-7368', 'KOI-7913'
+    ]
+    oversampling_factors = [
+        #5, 5, 9, 9
+        5,5
     ]
 
-    for star_id in star_ids:
+    for star_id, oversampling_factor in zip(star_ids, oversampling_factors):
 
         lcdir = os.path.join(PHOTDIR, star_id)
 
@@ -38,14 +43,16 @@ def main():
                     'window_length':window_length}
 
         cachepath = os.path.join(LOCALDIR, f"{star_id}_planetsearch.pkl")
+        #nworkers = 1 # FIXME FIXME
         r, search_time, search_flux, dtr_stages_dict = (
             run_periodograms_and_detrend(
                 star_id, time, flux, dtr_dict, n_threads=nworkers,
                 magisflux=True, return_extras=True, cachepath=cachepath,
-                period_min=1, period_max=100,
-                R_star_min=0.7, R_star_max=1.,
-                M_star_min=0.7, M_star_max=1.,
-                n_transits_min=4, oversampling_factor=5,
+                period_min=5, period_max=50,
+                R_star_min=0.8, R_star_max=1.0,
+                M_star_min=0.8, M_star_max=1.0,
+                n_transits_min=3, oversampling_factor=oversampling_factor,
+                transit_depth_min=10e-6, # 50 ppm
                 verbose=True
             )
         )
@@ -59,6 +66,10 @@ def main():
         )
 
         plot_tls_results(star_id, outdir, cachepath, dtr_dict)
+
+        with open(cachepath, 'rb') as f:
+            d = pickle.load(f)
+        import IPython; IPython.embed()
 
 
 if __name__ == "__main__":

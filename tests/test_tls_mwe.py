87c34216afc8e8468d03e08d28528c1fb9b13056
lgbouma <bouma.luke@gmail.com>
2022-05-23 12:20:20 -0400

    TLS MWE: working

--- tests/test_tls_mwe.py
+++ tests/test_tls_mwe.py
@@ -3,6 +3,22 @@ from transitleastsquares import transitleastsquares
 import multiprocessing as mp
 n_threads = mp.cpu_count()
 
+def p2p_rms(flux):
+    dflux = np.diff(flux)
+    med_dflux = np.nanmedian(dflux)
+    up_p2p = (
+        np.nanpercentile( np.sort(dflux-med_dflux), 84 )
+        -
+        np.nanpercentile( np.sort(dflux-med_dflux), 50 )
+    )
+    lo_p2p = (
+        np.nanpercentile( np.sort(dflux-med_dflux), 50 )
+        -
+        np.nanpercentile( np.sort(dflux-med_dflux), 16 )
+    )
+    p2p = np.mean([up_p2p, lo_p2p])
+    return p2p
+
 csvpath = 'KOI-7368_npoints50000.csv'
 df = pd.read_csv(csvpath)
 time, flux = np.array(df.time), np.array(df.flux)
@@ -13,7 +29,8 @@ flux = sigma_clip(flux, sigma_upper=3, sigma_lower=3)
 stime = time[~flux.mask]
 sflux = flux[~flux.mask]
 
-model = transitleastsquares(stime, sflux, verbose=True)
+dy = np.ones_like(stime)*p2p_rms(sflux)
+model = transitleastsquares(stime, sflux, dy=dy, verbose=True)
 results = model.power(
     use_threads=n_threads, show_progress_bar=True, R_star=0.88,
     R_star_min=0.85, R_star_max=0.91, M_star=0.88, M_star_min=0.85,
@@ -35,24 +52,19 @@ y_second_run = sflux[~intransit]
 t_second_run = stime[~intransit]
 t_second_run, y_second_run = cleaned_array(t_second_run, y_second_run)
 
-model2 = transitleastsquares(t_second_run, y_second_run, verbose=True)
+dy = np.ones_like(t_second_run)*p2p_rms(y_second_run)
+model2 = transitleastsquares(t_second_run, y_second_run, dy=dy, verbose=True)
 results2 = model2.power(
     use_threads=n_threads, show_progress_bar=True, R_star=0.88,
     R_star_min=0.85, R_star_max=0.91, M_star=0.88, M_star_min=0.85,
     M_star_max=0.91, period_min=5, period_max=10, n_transits_min=10,
     transit_template='default', transit_depth_min=10e-6, oversampling_factor=5
 )
-# raises the following warning:
-# /home/lbouma/miniconda3/envs/py37/lib/python3.7/site-packages/transitleastsquares/main.py:205:
-# UserWarning: No transit were fit. Try smaller "transit_depth_min"
 
-print(results['period'])
-# gives nan! (since the chi^2 = 46,409 == the number of data points for all
-# proposed periods)
+print(results2['period'])
 
 plt.close("all")
 plt.plot(results2['periods'], results2['power'], lw=0.5)
 plt.xlabel('period [days]')
 plt.ylabel('power')
 plt.savefig('tls_mwe_sigclip_run2.png', dpi=400)
-

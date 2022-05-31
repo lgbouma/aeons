dbe0e33154d538b3554ed12a60c9e7dadf75acac
lgbouma <bouma.luke@gmail.com>
2022-05-31 14:16:42 -0400

    run_cepher_koi_injectrecovery: cache success/fail

--- drivers/run_cepher_koi_injectrecovery.py
+++ drivers/run_cepher_koi_injectrecovery.py
@@ -1,6 +1,38 @@
 """
-
+Grid over:
+    _r_pl = np.array([16, 8, 6, 4, 3, 2, 1])*u.R_earth
+    _pl_orbper = [2, 4, 8, 16, 32, 64]
+how well can you recover the injected signals?
 """
+#############
+## LOGGING ##
+#############
+
+import logging
+from astrobase import log_sub, log_fmt, log_date_fmt
+
+DEBUG = False
+if DEBUG:
+    level = logging.DEBUG
+else:
+    level = logging.INFO
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(
+    level=level,
+    style=log_sub,
+    format=log_fmt,
+    datefmt=log_date_fmt,
+)
+
+LOGDEBUG = LOGGER.debug
+LOGINFO = LOGGER.info
+LOGWARNING = LOGGER.warning
+LOGERROR = LOGGER.error
+LOGEXCEPTION = LOGGER.exception
+
+###########
+# IMPORTS #
+###########
 import os, pickle
 import numpy as np, matplotlib.pyplot as plt, pandas as pd
 from astropy import units as u
@@ -24,19 +56,24 @@ def main():
 
     # r_star, m_star
     star_dict = {
-        'Kepler-1627': [(0.881, 0.018), (0.953, 0.019)],
         'Kepler-1643': [(0.855, 0.044), (0.845, 0.025)],
+        'Kepler-1627': [(0.881, 0.018), (0.953, 0.019)],
         'KOI-7368': [(0.876, 0.035), (0.879, 0.018)],
         'KOI-7913': [(0.790, 0.049), (0.760, 0.025)],
     }
 
-    #_r_pl = np.array([1, 2, 3, 4, 6, 8, 16])*u.R_earth
     _r_pl = np.array([16, 8, 6, 4, 3, 2, 1])*u.R_earth
     _pl_orbper = [2, 4, 8, 16, 32, 64]
 
     for _star_id in list(star_dict.keys()):
 
         for r_pl, pl_orbper in product(_r_pl, _pl_orbper):
+
+            star_id = _star_id + f"-synth-P{pl_orbper}d-Rp{int(r_pl.value)}"
+            search_method = 'tls'
+            cachepath = os.path.join(
+                LOCALDIR, f"{star_id}_{search_method}_iterative_search.pkl"
+            )
 
             lcdir = os.path.join(PHOTDIR, _star_id)
             if not os.path.exists(lcdir): os.mkdir(lcdir)
@@ -49,23 +86,23 @@ def main():
             # injection-related steps
             r_star = star_dict[_star_id][0][0]*u.Rsun
             rp_rs = (r_pl / r_star).cgs.value
+            np.random.seed(42)
             epoch = np.nanmin(time)+np.random.uniform(low=0,high=pl_orbper)
             depth = rp_rs**2
             inj_dict = {
+                'star_id': _star_id,
                 'period':pl_orbper,
                 'epoch':epoch,
-                'depth':depth
+                'depth':depth,
+                'r_pl':r_pl.to(u.Rearth).value,
+                'r_star':r_star.to(u.Rsun).value,
+                'rp_rs':rp_rs
             }
             time, flux, _ = lcu.inject_transit_signal(time, flux, inj_dict)
-            star_id = _star_id + f"-synth-P{pl_orbper}d-Rp{int(r_pl.value)}"
 
             dtr_dict = {
                 'method':'best', 'break_tolerance':0.5, 'window_length':0.5
             }
-            search_method = 'tls'
-            cachepath = os.path.join(
-                LOCALDIR, f"{star_id}_{search_method}_iterative_search.pkl"
-            )
 
             R_star = star_dict[_star_id][0][0]
             R_star_min = star_dict[_star_id][0][0] - 4*star_dict[_star_id][0][1]
@@ -92,6 +129,18 @@ def main():
             outdir = os.path.join(RESULTSDIR, 'cepher_koi_injectrecovery')
             if not os.path.exists(outdir): os.mkdir(outdir)
 
+            recov, weakrecov = lcu.determine_if_recovered(cachepath, inj_dict)
+            inj_dict['recovered'] = recov
+            inj_dict['partial_recovered'] = weakrecov
+
+            outdf = pd.DataFrame(inj_dict, index=[0])
+            outpath = os.path.join(
+                RESULTSDIR, "cepher_koi_injectrecovery",
+                f"{star_id}_{search_method}_recovery_result.csv"
+            )
+            outdf.to_csv(outpath, index=False)
+            LOGINFO(f'made {outpath}')
+
             dtr_stages_dict = outdicts['dtr_stages_dict']
             plot_detrend_check(
                 star_id, outdir, dtr_dict,
@@ -102,10 +151,6 @@ def main():
                 star_id, search_method, outdir, cachepath, dtr_dict,
                 overwrite=True
             )
-
-            #TODO: add some log file of whether the recovery succeeded!
-            # with open(cachepath, 'rb') as f:
-            #     d = pickle.load(f)
 
 
 if __name__ == "__main__":
